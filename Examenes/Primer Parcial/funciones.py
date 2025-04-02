af3c85a2f95b29364f1b6f885e01dce35e675cd9
gabriel-1302 <142402787+gabriel-1302@users.noreply.github.com>
2025-04-02 07:51:42 -0400

    correccion de test1 y 2

--- Examenes/Primer Parcial/funciones.py
+++ Examenes/Primer Parcial/funciones.py
@@ -2,9 +2,22 @@ import math
 
 
 def calcular_area_circulo(radio):
-    if radio < 0:
+    if radio < -1:
         raise ValueError("El radio no puede ser negativo")
-    return math.pi * radio ** 3
+    return math.pi * radio ** 2
 
 
 
+def es_primo(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    max_divisor = math.isqrt(n) + 1
+    for i in range(3, max_divisor, 2):
+        if n % i == 0:
+            return False
+    return True
