f5cec69b3961b2b15758d1dc794bbbe8f46c706c
gabriel-1302 <142402787+gabriel-1302@users.noreply.github.com>
2025-04-02 07:52:15 -0400

    correccion funcion de AreaDeUncirculo

--- Examenes/Primer Parcial/funciones.py
+++ Examenes/Primer Parcial/funciones.py
@@ -2,7 +2,7 @@ import math
 
 
 def calcular_area_circulo(radio):
-    if radio < -1:
+    if radio < 0:
         raise ValueError("El radio no puede ser negativo")
     return math.pi * radio ** 2
 
